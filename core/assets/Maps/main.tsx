0a5b8f8b9af8d4cf9d02ea365d3bd6364f5b603e
codeclanstudent <calum.rogers@ntlworld.com>
2021-12-02 16:18:25 +0000

    still trying to get collision to work

---
+++ core/assets/Maps/main.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="main" tilewidth="16" tileheight="16" tilecount="8000" columns="200">
+ <image source="../Ship.png" width="3200" height="640"/>
+</tileset>
